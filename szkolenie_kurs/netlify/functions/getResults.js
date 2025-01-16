1cc3b19e3280aefedac9db2b6bc6105e6ebc96b3
karolsobon <karolsobon@aol.com>
2025-01-16 18:04:49 +0100

    uri

--- szkolenie_kurs/netlify/functions/getResults.js
+++ szkolenie_kurs/netlify/functions/getResults.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     if (mongoose.connection.readyState === 0) {
-        await mongoose.connect(process.env.MONGOOSE);
+        await mongoose.connect(process.env.MANGOOSE);
     }
 };
 
