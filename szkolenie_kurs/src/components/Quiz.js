87506c398c55e4c10cadfcf28dc5f709bee4e431
karolsobon <karolsobon@aol.com>
2025-03-03 18:28:49 +0100

    quiz time

--- szkolenie_kurs/src/components/Quiz.js
+++ szkolenie_kurs/src/components/Quiz.js
@@ -49,7 +49,7 @@ const Description = styled.div`
   border-radius: 10px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
   max-width: 700px;
-  
+
   p {
     margin-bottom: 12px;
     font-weight: 500;
@@ -238,207 +238,207 @@ const Quiz = () => {
         "Międzynarodowy plan płatności",
         "Międzywydziałowy program podatkowy",
         "Międzynarodowy pakt podatkowy",
-        "Mechanizm podzielonej płatności",
-      ],
-      correct: 3,
-    },
-    {
-      question:
-        "11. Co oznacza bardzo powszechnie stosowany w księgowości skrót 'RB'?",
-      answers: [
-        "Rachunek bankowy",
-        "Raport bieżący",
-        "Roczny bilans",
-        "Rejestracja biznesu",
-      ],
-      correct: 0,
-    },
-    {
-      question: "12. Co oznacza skrót 'MPK'?",
-      answers: [
-        "Międzynarodowy plan kont",
-        "Miejsce przechowywania ksiąg",
-        "Miejsce powstawania kosztu",
-        "Międzynarodowy Pakt Księgowy",
-      ],
-      correct: 2,
-    },
-    {
-      question: "13. Co oznacza skrót 'WZ'?",
-      answers: [
-        "Weryfikacja zasobów",
-        "Wartość zestawienia",
-        "Wewnętrzne zestawienie",
-        "Wydanie zewnętrzne",
-      ],
-      correct: 3,
-    },
-    {
-      question: "14. Co ukrywa się pod skrótem 'RZiS'?",
-      answers: [
-        "Rejestr Zabezpieczeń i Środków",
-        "Raport Zbiorczy i Szczegółowy",
-        "Rachunek Zysków i Strat",
-        "Rejestr Zmian i Stanów",
-      ],
-      correct: 2,
-    },
-    {
-      question: "15. Co oznaczają skróty 'BO' oraz 'BZ'?",
-      answers: [
-        "Biuro Obsługi, Baza Zasobów",
-        "Bieżąca Ocena, Biuro Zleceniobiorcy",
-        "Bilans Otwarcia, Bilans Zamknięcia",
-        "Baza Operacyjna, Bieżący Zysk",
-      ],
-      correct: 2,
-    },
-    {
-      question: "16. Na fakturze VAT nie musi znajdować się:",
-      answers: ["Numer NIP Firmy", "Numer Regon Firmy", "Adres Firmy"],
-      correct: 1,
-    },
-    {
-      question: "17. Faktura pro forma wymaga podpisu:",
-      answers: [
-        "tylko w formie papierowej",
-        "zarówno w formie papierowej, jak i cyfrowej",
-        "nie wymaga podpisu",
-      ],
-      correct: 2,
-    },
-    {
-      question:
-        "18. Jeśli prowadzisz jednoosobową działalność gospodarczą, która nie ponosi dużych kosztów, najkorzystniejszą formą opodatkowania może się okazać:",
-      answers: [
-        "ryczałt od przychodów ewidencjonowanych",
-        "Księga Przychodów i Rozchodów",
-        "pełna księgowość",
-      ],
-      correct: 0,
-    },
-    {
-      question:
-        "19. Ile czasu, licząc od dnia bilansowego, mają spółki zarejestrowane w Krajowym Rejestrze Sądowym (KRS) na sporządzenie sprawozdania:",
-      answers: ["3 miesiące", "2 miesiące", "1 miesiąc"],
-      correct: 0,
-    },
-    {
-      question:
-        "20. Jaki próg rocznej sprzedaży zobowiązuje firmę do rejestracji jako płatnik VAT:",
-      answers: ["300 tys. Zł", "200 tys. Zł", "500 tys. Zł"],
-      correct: 1,
-    },
-    {
-      question:
-        "21. Jednolity Plik Kontrolny (JPK) to cyfrowy dokument zawierający zestaw danych firmowych, a obowiązek jego składania jest nałożony na każdego:",
-      answers: [
-        "zgłaszającego ubezpieczonych w ZUS",
-        "prowadzącego działalność gospodarczą",
-        "czynnego podatnika VAT",
-      ],
-      correct: 2,
-    },
-    {
-      question: "22. Z płacenia podatku PIT są zwolnione:",
-      answers: [
-        "duchowni katoliccy i innych wyznań",
-        "osoby do 26 roku życia, które nie osiągają dochodów przekraczających 85 528 zł rocznie",
-        "rolnicy ubezpieczeni w KRUS",
-      ],
-      correct: 1,
-    },
-    {
-      question:
-        "23. Dokumenty podlegające księgowaniu przez jednostkę gospodarczą to dowody:",
-      answers: ["budżetowe", "materialne", "księgowe", "pieniężne"],
-      correct: 2,
-    },
-    {
-      question:
-        "24. Bilans, czyli główny element sprawozdania finansowego, składa się z pozycji:",
-      answers: [
-        "aktywa i pasywa",
-        "przychody i koszty",
-        "zyski i straty",
-        "koszty operacyjne i przychody finansowe",
-      ],
-      correct: 0,
-    },
-    {
-      question:
-        "25. Podstawowym dokumentem regulującym w Polsce system rachunkowości jest:",
-      answers: [
-        "Ustawa o podatku dochodowym od osób fizycznych",
-        "Ustawa o podatku dochodowym od osób prawnych",
-        "Ustawa o swobodzie działalności gospodarczej",
-        "Ustawa o rachunkowości",
-      ],
-      correct: 3,
-    },
-    {
-      question: "26. Wprowadzenie do sprawozdania finansowego to część:",
-      answers: [
-        "bilansu",
-        "rachunku zysków i strat",
-        "informacji dodatkowej",
-        "rachunku przepływów pieniężnych",
-      ],
-      correct: 2,
-    },
-    {
-      question: "27. Do głównych elementów sprawozdania finansowego zaliczamy:",
-      answers: [
-        "bilans, rachunek zysków i strat, informację dodatkową",
-        "rachunek zysków i strat, informację dodatkową, zestawienie zmian w kapitale (funduszu) własnym",
-        "bilans, rachunek przepływów pieniężnych, informację dodatkową",
-        "bilans, rachunek zysków i strat, rachunek przepływów pieniężnych",
-      ],
-      correct: 3,
-    },
-    {
-      question:
-        "28. Zamknięcie ksiąg rachunkowych na koniec roku obrotowego polega na:",
-      answers: [
-        "dokonaniu inwentaryzacji",
-        "ustaleniu sald końcowych aktywów i pasywów bilansu",
-        "porównaniu zysków i start minionego okresu",
-        "dokonaniu spisu dokumentów księgowych z danego okresu",
-      ],
-      correct: 1,
-    },
-    {
-      question: "29. Inwentaryzacją nazywamy:",
-      answers: [
-        "opublikowanie sprawozdania finansowego",
-        "ustalanie faktycznego stanu aktywów i pasywów",
-        "badanie sprawozdania finansowego",
-        "zliczanie dowodów księgowych",
-      ],
-      correct: 1,
-    },
-    {
-      question: "30. Otwarcia ksiąg rachunkowych dokonuje się:",
-      answers: [
-        "na dzień zmiany zarządu",
-        "na początek każdego miesiąca",
-        "przed walnym zgromadzeniem zarządu",
-        "na dzień ogłoszenia upadłości",
-      ],
-      correct: 3,
-    },
-    {
-      question:
-        "31. W którym roku powołano ogólnopolski 'Związek Księgowych w Polsce'?",
-      answers: ["1922", "1926", "1932"],
-      correct: 1,
-    },
-    {
-      question:
-        "32. Kiedy i na podstawie czyjej decyzji zostało zarejestrowane Stowarzyszenie Księgowych w Polsce (SKwP) po zakończeniu wojny?",
-      answers: [
-        "15 stycznia 1945 roku, na podstawie decyzji Premiera Polski",
-        "30 lipca 1946 roku, na podstawie decyzji Prezydenta miasta stołecznego Warszawy",
+        "Mechanizm dzielonej płatności",
+],
+correct: 3,
+},
+{
+question:
+"11. Co oznacza bardzo powszechnie stosowany w księgowości skrót 'RB'?",
+answers: [
+"Rachunek bankowy",
+"Raport bieżący",
+"Roczny bilans",
+"Rejestracja biznesu",
+],
+correct: 0,
+},
+{
+question: "12. Co oznacza skrót 'MPK'?",
+answers: [
+"Międzynarodowy plan kont",
+"Miejsce przechowywania ksiąg",
+"Miejsce powstawania kosztu",
+"Międzynarodowy Pakt Księgowy",
+],
+correct: 2,
+},
+{
+question: "13. Co oznacza skrót 'WZ'?",
+answers: [
+"Weryfikacja zasobów",
+"Wartość zestawienia",
+"Wewnętrzne zestawienie",
+"Wydanie zewnętrzne",
+],
+correct: 3,
+},
+{
+question: "14. Co ukrywa się pod skrótem 'RZiS'?",
+answers: [
+"Rejestr Zabezpieczeń i Środków",
+"Raport Zbiorczy i Szczegółowy",
+"Rachunek Zysków i Strat",
+"Rejestr Zmian i Stanów",
+],
+correct: 2,
+},
+{
+question: "15. Co oznaczają skróty 'BO' oraz 'BZ'?",
+answers: [
+"Biuro Obsługi, Baza Zasobów",
+"Bieżąca Ocena, Biuro Zleceniobiorcy",
+"Bilans Otwarcia, Bilans Zamknięcia",
+"Baza Operacyjna, Bieżący Zysk",
+],
+correct: 2,
+},
+{
+question: "16. Na fakturze VAT nie musi znajdować się:",
+answers: ["Numer NIP Firmy", "Numer Regon Firmy", "Adres Firmy"],
+correct: 1,
+},
+{
+question: "17. Faktura pro forma wymaga podpisu:",
+answers: [
+"tylko w formie papierowej",
+"zarówno w formie papierowej, jak i cyfrowej",
+"nie wymaga podpisu",
+],
+correct: 2,
+},
+{
+question:
+"18. Jeśli prowadzisz jednoosobową działalność gospodarczą, która nie ponosi dużych kosztów, najkorzystniejszą formą opodatkowania może się okazać:",
+answers: [
+"ryczałt od przychodów ewidencjonowanych",
+"Księga Przychodów i Rozchodów",
+"pełna księgowość",
+],
+correct: 0,
+},
+{
+question:
+"19. Ile czasu, licząc od dnia bilansowego, mają spółki zarejestrowane w Krajowym Rejestrze Sądowym (KRS) na sporządzenie sprawozdania:",
+answers: ["3 miesiące", "2 miesiące", "1 miesiąc"],
+correct: 0,
+},
+{
+question:
+"20. Jaki próg rocznej sprzedaży zobowiązuje firmę do rejestracji jako płatnik VAT:",
+answers: ["300 tys. Zł", "200 tys. Zł", "500 tys. Zł"],
+correct: 1,
+},
+{
+question:
+"21. Jednolity Plik Kontrolny (JPK) to cyfrowy dokument zawierający zestaw danych firmowych, a obowiązek jego składania jest nałożony na każdego:",
+answers: [
+"zgłaszającego ubezpieczonych w ZUS",
+"prowadzącego działalność gospodarczą",
+"czynnego podatnika VAT",
+],
+correct: 2,
+},
+{
+question: "22. Z płacenia podatku PIT są zwolnione:",
+answers: [
+"duchowni katoliccy i innych wyznań",
+"osoby do 26 roku życia, które nie osiągają dochodów przekraczających 85 528 zł rocznie",
+"rolnicy ubezpieczeni w KRUS",
+],
+correct: 1,
+},
+{
+question:
+"23. Dokumenty podlegające księgowaniu przez jednostkę gospodarczą to dowody:",
+answers: ["budżetowe", "materialne", "księgowe", "pieniężne"],
+correct: 2,
+},
+{
+question:
+"24. Bilans, czyli główny element sprawozdania finansowego, składa się z pozycji:",
+answers: [
+"aktywa i pasywa",
+"przychody i koszty",
+"zyski i straty",
+"koszty operacyjne i przychody finansowe",
+],
+correct: 0,
+},
+{
+question:
+"25. Podstawowym dokumentem regulującym w Polsce system rachunkowości jest:",
+answers: [
+"Ustawa o podatku dochodowym od osób fizycznych",
+"Ustawa o podatku dochodowym od osób prawnych",
+"Ustawa o swobodzie działalności gospodarczej",
+"Ustawa o rachunkowości",
+],
+correct: 3,
+},
+{
+question: "26. Wprowadzenie do sprawozdania finansowego to część:",
+answers: [
+"bilansu",
+"rachunku zysków i strat",
+"informacji dodatkowej",
+"rachunku przepływów pieniężnych",
+],
+correct: 2,
+},
+{
+question: "27. Do głównych elementów sprawozdania finansowego zaliczamy:",
+answers: [
+"bilans, rachunek zysków i strat, informację dodatkową",
+"rachunek zysków i strat, informację dodatkową, zestawienie zmian w kapitale (funduszu) własnym",
+"bilans, rachunek przepływów pieniężnych, informację dodatkową",
+"bilans, rachunek zysków i strat, rachunek przepływów pieniężnych",
+],
+correct: 3,
+},
+{
+question:
+"28. Zamknięcie ksiąg rachunkowych na koniec roku obrotowego polega na:",
+answers: [
+"dokonaniu inwentaryzacji",
+"ustaleniu sald końcowych aktywów i pasywów bilansu",
+"porównaniu zysków i start minionego okresu",
+"dokonaniu spisu dokumentów księgowych z danego okresu",
+],
+correct: 1,
+},
+{
+question: "29. Inwentaryzacją nazywamy:",
+answers: [
+"opublikowanie sprawozdania finansowego",
+"ustalanie faktycznego stanu aktywów i pasywów",
+"badanie sprawozdania finansowego",
+"zliczanie dowodów księgowych",
+],
+correct: 1,
+},
+{
+question: "30. Otwarcia ksiąg rachunkowych dokonuje się:",
+answers: [
+"na dzień zmiany zarządu",
+"na początek każdego miesiąca",
+"przed walnym zgromadzeniem zarządu",
+"na dzień ogłoszenia upadłości",
+],
+correct: 3,
+},
+{
+question:
+"31. W którym roku powołano ogólnopolski 'Związek Księgowych w Polsce'?",
+answers: ["1922", "1926", "1932"],
+correct: 1,
+},
+{
+question:
+"32. Kiedy i na podstawie czyjej decyzji zostało zarejestrowane Stowarzyszenie Księgowych w Polsce (SKwP) po zakończeniu wojny?",
+answers: [
+"15 stycznia 1945 roku, na podstawie decyzji Premiera Polski",
+"30 lipca 1946 roku, na podstawie decyzji Prezydenta miasta stołecznego Warszawy",
         "12 kwietnia 1947 roku, na podstawie decyzji Ministra Finansów",
       ],
       correct: 1,
@@ -573,7 +573,6 @@ const Quiz = () => {
   };
 
   const calculateScore = () => {
-    // Moved this function definition UP
     return userAnswers.reduce(
       (score, answer, index) =>
         answer === questions[index].correct ? score + 1 : score,
@@ -582,7 +581,7 @@ const Quiz = () => {
   };
 
   const handleSubmit = async () => {
-    setIsLoading(true); // Włączenie animacji oczekiwania
+    setIsLoading(true);
 
     const unansweredQuestions = userAnswers.reduce((acc, answer, index) => {
       if (answer === null) {
@@ -592,7 +591,6 @@ const Quiz = () => {
     }, []);
 
     if (unansweredQuestions.length > 0) {
-      // Przeniesienie do pierwszego niewypełnionego pytania
       const firstUnansweredQuestion = document.querySelector(
         `#question-${unansweredQuestions[0]}`
       );
@@ -600,7 +598,6 @@ const Quiz = () => {
         firstUnansweredQuestion.scrollIntoView({ behavior: "smooth" });
       }
 
-      // Podświetlenie pytań, na które nie udzielono odpowiedzi
       unansweredQuestions.forEach((questionIndex) => {
         const questionElement = document.querySelector(
           `#question-${questionIndex}`
@@ -611,10 +608,12 @@ const Quiz = () => {
       });
     } else {
       const score = calculateScore();
+      const timestamp = Date.now(); // Dodanie znacznika czasu
+
       try {
         await axios.post(
           "https://szkoleniekostarskak.netlify.app/.netlify/functions/saveResults",
-          { nickname, score }
+          { nickname, score, timestamp } // Dołączenie znacznika czasu
         );
         setIsSubmitted(true);
         localStorage.setItem("quizSubmitted", "true");
@@ -623,7 +622,7 @@ const Quiz = () => {
       }
     }
 
-    setIsLoading(false); // Wyłączenie animacji oczekiwania
+    setIsLoading(false);
   };
 
   return (
@@ -633,46 +632,46 @@ const Quiz = () => {
         <Overlay>
           <Spinner />
         </Overlay>
-      )}
-      {isSubmitted ? (
+        )}
+        {isSubmitted ? (
         <Message>Odpowiedzi zostały wysłane.</Message>
-      ) : (
+        ) : (
         <QuizBox>
-          <Title>Quiz dla {nickname}</Title>
-          <Description>
-            <p>Witaj! Przed Tobą test sprawdzający wiedzę z księgowości.</p>
-            <p>Składa się z 44 pytań zamkniętych, bez limitu czasu.</p>
-            <p>
-              Test pomoże ocenić Twoje umiejętności i wskaże obszary do dalszej
-              nauki.
-            </p>
-            <p>Nie przejmuj się wynikiem – to tylko wskazówka do rozwoju!</p>
-          </Description>
-
-          {questions.map((q, questionIndex) => (
-            <div key={questionIndex} id={`question-${questionIndex}`}>
-              <Question>{q.question}</Question>
-              {q.answers.map((answer, answerIndex) => (
-                <AnswerLabel key={answerIndex}>
-                  <input
-                    type="radio"
-                    name={`question-${questionIndex}`}
-                    value={answerIndex}
-                    checked={userAnswers[questionIndex] === answerIndex}
-                    onChange={() =>
-                      handleAnswerChange(questionIndex, answerIndex)
-                    }
-                  />
-                  {answer}
-                </AnswerLabel>
+        <Title>Quiz dla {nickname}</Title>
+        <Description>
+        <p>Witaj! Przed Tobą test sprawdzający wiedzę z księgowości.</p>
+        <p>Składa się z 44 pytań zamkniętych, bez limitu czasu.</p>
+        <p>
+        Test pomoże ocenić Twoje umiejętności i wskaże obszary do dalszej
+        nauki.
+        </p>
+        <p>Nie przejmuj się wynikiem – to tylko wskazówka do rozwoju!</p>
+        </Description>
+        
+              {questions.map((q, questionIndex) => (
+                <div key={questionIndex} id={`question-${questionIndex}`}>
+                  <Question>{q.question}</Question>
+                  {q.answers.map((answer, answerIndex) => (
+                    <AnswerLabel key={answerIndex}>
+                      <input
+                        type="radio"
+                        name={`question-${questionIndex}`}
+                        value={answerIndex}
+                        checked={userAnswers[questionIndex] === answerIndex}
+                        onChange={() =>
+                          handleAnswerChange(questionIndex, answerIndex)
+                        }
+                      />
+                      {answer}
+                    </AnswerLabel>
+                  ))}
+                </div>
               ))}
-            </div>
-          ))}
-          <Button onClick={handleSubmit}>Zakończ test</Button>
-        </QuizBox>
-      )}
-    </Container>
-  );
-};
-
-export default Quiz;
+              <Button onClick={handleSubmit}>Zakończ test</Button>
+            </QuizBox>
+          )}
+        </Container>
+        );
+        };
+        
+        export default Quiz;
